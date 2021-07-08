777c3235dd7f887bba5363ec5abae103ca46a5c2
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-08 11:01:25 -0300

    Cria Modelo Jogo

---
+++ ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiCatalogoJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : Controller
+    {
+        //GET
+        [HttpGet]
+        public async Task<ActionResult<List<JogoViewModel>>> Obter()
+        {
+            return Ok();
+        }
+        
+        //GET
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<List<JogoViewModel>>> Obter(Guid guid)
+        {
+            return Ok();
+        }
+        
+        //POST
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> IserirJogo(JogoInputModel jogo)
+        {
+            return Ok();
+        }
+        
+        //PUT
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid guid, JogoInputModel jogo)
+        {
+            return Ok();
+        }
+        
+        //PATCH
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid guid, double preco, object jogo)
+        {
+            return Ok();
+        }
+        
+        //DELETE
+        [HttpDelete]
+        public async Task<ActionResult> DeletarJogo(Guid guid)
+        {
+            return Ok();
+        }
+    }
+}
